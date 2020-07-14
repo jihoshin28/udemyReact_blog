c30915bc85a93ad982283638b6f99682b66b14f3
geeuho <geeuho@gmail.com>
2020-07-14 15:24:09 -0700

    added reducers api and actions folders with redux-thunk async

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
+import App from './components/App'
+import thunk from 'redux-thunk'
+
+let store = createStore(reducers, applyMiddleware(thunk))
+ReactDOM.render(
+    <Provider store = {createStore(store)}>
+        <App/>
+    </Provider>, document.querySelector('#root')
+)
